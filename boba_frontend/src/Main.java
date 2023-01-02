bfcc1edd2b6ddd8a525a5b76e0ec92f798b0616a
vishalkantharaju <112344056+vishalkantharaju@users.noreply.github.com>
2023-01-02 14:50:29 -0600

    Add files via upload

---
+++ boba_frontend/src/Main.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.io.FileNotFoundException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        GUI screen = new GUI();
+
+    }
+
+}
